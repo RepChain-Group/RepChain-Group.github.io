c1bd51912dfbd92929108c404c071fd93e9c7cb6
taixingyiji <1a2b3C>
2022-05-09 15:16:44 +0800

    feat: remove useless menu

--- docs/.vuepress/config/navbar.ts
+++ docs/.vuepress/config/navbar.ts
@@ -38,13 +38,6 @@ export const navbar = [
       }
     ]
   },
-  {
-    text: '版本',
-    children: [
-      { text: '2.x(alpha)', link: 'http://v2.vuepress-reco.recoluan.com/' },
-      { text: '1.x', link: 'https://vuepress-theme-reco.recoluan.com/views/1.x/' },
-    ],
-  },
   // { text: '留言板', link: '/docs/message-board' },
   { text: 'Git', link: 'https://gitee.com/BTAJL/repchain' },
 ]
