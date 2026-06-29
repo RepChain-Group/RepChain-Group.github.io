b279ac12bd53ebc5568b8226cf7dfdd8056319ed
zls201624 <737016271@qq.com>
2026-06-29 16:17:18 +0800

    update head word

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -2,7 +2,7 @@ export const siteData = {
   "base": "/",
   "lang": "en-US",
   "title": "RepChain",
-  "description": "RepChain（Reactive Permissioned Chain）是第一款采用响应式编程实现的自主可控的区块链基础组件。由中国科学院软件研究所、贵阳信息技术研究院、中科软科技股份有限公司和北京连琪科技有限公司共同研发，支持单位有广州软件应用技术研究院、中科嘉速、中科金审和广州中科易德。RepChain已通过工信部信通院“2019、2020可信区块链”功能测试和性能测试。",
+  "description": "RepChain（Reactive Permissioned Chain）是第一款采用响应式编程实现的自主可控的区块链基础组件。由中国科学院软件研究所、贵阳信息技术研究院、中科软科技股份有限公司和北京连琪科技有限公司共同研发，支持单位有中科嘉速、中科智城（广州）信息科技有限公司。RepChain已通过工信部信通院“2019、2020可信区块链”功能测试和性能测试。",
   "head": [],
   "locales": {}
 }
